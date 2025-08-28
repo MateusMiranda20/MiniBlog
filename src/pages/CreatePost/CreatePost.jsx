b94ce7ab86d0a3a9fea29048631376a8bc8f1f97
MateusMiranda20 <mateuusmiranda20@gmail.com>
2025-08-28 18:17:23 -0300

    Validating some fields. and tweaking the styling

--- src/pages/CreatePost/CreatePost.jsx
+++ src/pages/CreatePost/CreatePost.jsx
@@ -1,16 +1,59 @@
 import styles from "../CreatePost/CreatPost.module.css"
 import { useState } from "react"
 
+import { useNavigate } from "react-router-dom"
+import { useAuthValue } from "../../context/AuthContext"
+import { useInsertDocument } from "../../hooks/useInsertDocument"
+
 function CreatePost() {
     const [title, setTitle] = useState("")
     const [image, setImage] = useState("")
     const [body, setBody] = useState("")
-    const [tags, setTags] = useState([])
-    //const [formError, setFormError] = useState("")
+    const [tags, setTags] = useState("")
+    const [formError, setFormError] = useState("")
+
+    const { user } = useAuthValue()
+
+    const { insertDocument, response } = useInsertDocument("posts")
 
     const handleSubmit = (e) => {
         e.preventDefault()
-    } 
+        setFormError("")
+
+        const tagsArray = tags.split(",").map(tag => tag.trim().toLowerCase())
+
+
+        //Validate image URL
+        if (!image.match(/\.(jpeg|jpg|gif|png)$/)) {
+            setFormError("Por favor, insira uma URL de imagem válida.")
+            return
+        }
+
+
+        //Chamar a função de inserção de documento
+        //insertDocument({
+        //   title,
+        //  image,
+        // body,
+        // tags: tagsArray
+        //});
+
+        //Checar todos os valores
+        //if (!title || !image || !body || tagsArray.length === 0) {
+        //  setFormError("Por favor, preencha todos os campos.")
+        //  return
+        //}
+
+        insertDocument({
+            title,
+            image,
+            body,
+            tags: tagsArray,
+            uid: user.uid,
+            createdBy: user.displayName
+        })
+
+    }
 
 
     return (
@@ -20,52 +63,55 @@ function CreatePost() {
             <form onSubmit={handleSubmit}>
                 <label>
                     <span>Titulo:</span>
-                    <input 
-                    type="text"
-                    name="title"
-                    required
-                    placeholder="Pense em um bom título..."
-                    onChange={(e) => setTitle(e.target.value)}
-                    value={title}
+                    <input
+                        type="text"
+                        name="title"
+                        required
+                        placeholder="Pense em um bom título..."
+                        onChange={(e) => setTitle(e.target.value)}
+                        value={title}
                     />
                 </label>
                 <label>
                     <span>Imagem:</span>
-                    <input 
-                    type="text"
-                    name="image"
-                    required
-                    placeholder="Insira a URL da imagem..."
-                    onChange={(e) => setImage(e.target.value)}
-                    value={image}
+                    <input
+                        type="text"
+                        name="image"
+                        required
+                        placeholder="Insira a URL da imagem..."
+                        onChange={(e) => setImage(e.target.value)}
+                        value={image}
                     />
                 </label>
                 <label>
                     <span>Conteúdo:</span>
                     <textarea
-                    name="body"
-                    required
-                    placeholder="Escreva o conteúdo do post..."
-                    onChange={(e) => setBody(e.target.value)}
-                    value={body}
+                        name="body"
+                        required
+                        placeholder="Escreva o conteúdo do post..."
+                        onChange={(e) => setBody(e.target.value)}
+                        value={body}
                     />
                 </label>
                 <label>
                     <span>Tags:</span>
-                    <input 
-                    type="text"
-                    name="tags"
-                    required
-                    placeholder="Adicione tags separadas por vírgula..."
-                    onChange={(e) => setTags(e.target.value.split(","))}
-                    value={tags}
+                    <input
+                        type="text"
+                        name="tags"
+                        required
+                        placeholder="Adicione tags separadas por vírgula..."
+                        onChange={(e) => setTags(e.target.value)}
+                        value={tags}
                     />
                 </label>
-                {/*
-                {!loading && <button className="btn">Cadastrar</button>}
-                    {loading && ( <button className="btn" disabled>Aguarde...</button>)}
-                    {error && <p className="error">{error}</p>} 
-                */}
+                {!response.loading && <button className="btn">Cadastrar</button>}
+                {response.loading &&(
+                    <button className="btn" disabled>
+                        Aguarde...
+                    </button>
+                )}
+                {response.error && <p className="error">{response.error}</p>}
+                {formError && <p className="error">{formError}</p>}
             </form>
         </div>
     )
