1d8249a088274f89414bb80d77c46b597b65020c
MateusMiranda20 <mateuusmiranda20@gmail.com>
2025-09-06 15:08:45 -0300

    editing my post css

--- src/pages/Post/Post.jsx
+++ src/pages/Post/Post.jsx
@@ -14,7 +14,7 @@ function Post() {
                 <>
                     <h1>{post.title}</h1>
                     <img src={post.image} alt={post.title} />
-                    <p>{post.body}</p>
+                    <p className={styles.body}>{post.body}</p>
                     <h3>Este post trata sobre:</h3>
                     <div className={styles.tags}>
                         {post.tags.map((tags)=>(
